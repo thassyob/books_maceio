26f92f30936407a607f63c246e4976a4b9b95eec
Thassyo <thassyob@gmail.com>
2022-07-05 22:33:50 -0300

    endpoint edit book

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,15 +1,26 @@
 class Api::V1::BooksController < ApplicationController
   def create
-    book = Book.create!(book_params)
+    book = Book.create!(create_params)
 
     render json: book,
            serializer: Api::V1::Books::Create::BookSerializer, 
            status: :created
   end
 
+  def update
+    book = Book.find(params[:id])
+
+    book.update!(update_params)
+    head :no_content
+  end
+
   private
 
-  def book_params
+  def create_params
+    params.require(:book).permit(:title, :description, :author, :image)
+  end
+
+  def update_params
     params.require(:book).permit(:title, :description, :author, :image)
   end
 end  
--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -18,4 +18,18 @@ RSpec.describe "Api::V1::Books", type: :request do
       end
     end
   end
+
+  describe 'PUT #update' do
+    context 'when pass valid data' do
+      it 'return 204 status code' do
+        user = create(:user)
+        book = create(:book)
+        book_params = { title: 'Teste', author: 'teste' }
+
+        put "/api/v1/books/#{book.id}", params: { book: book_params }, headers: get_headers(user)
+
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+  end
 end 
