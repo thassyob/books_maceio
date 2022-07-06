96b9c27aa6d5dc1340ac8702142e56aad8629df1
Thassyo <thassyob@gmail.com>
2022-07-06 00:54:40 -0300

    endpoint index books

--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -1,6 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Books", type: :request do
+  describe 'INDEX #books' do
+    context 'when list books' do
+      it 'must return 200 http status code and list attributes' do
+        user = create(:user)
+        book1 = create(:book, created_at: 1.days.ago)
+        book2 = create(:book, created_at: DateTime.current)
+
+        get '/api/v1/books', headers: get_headers(user)
+
+        expect(json_body[0]).to have_key(:id)
+        expect(json_body[0]).to have_key(:title)
+        expect(json_body[0]).to have_key(:description)
+        expect(json_body[0]).to have_key(:author)
+        expect(json_body[0]).to have_key(:image)
+
+        expect(json_body[0][:id]).to eq(book2.id)
+        expect(json_body[1][:id]).to eq(book1.id)
+      end
+    end
+
+    context 'when filtered list books' do
+      it 'by title' do
+        user = create(:user)
+        book1 = create(:book, title: 'teste1')
+        book2 = create(:book, title: 'teste2')
+        query_params = 'teste1'
+
+        get "/api/v1/books?title=#{query_params}", headers: get_headers(user)
+        
+        expect(json_body.count).to eq(1)
+      end
+
+      it 'by author' do
+        user = create(:user)
+        book1 = create(:book, author: 'teste1')
+        book2 = create(:book, author: 'teste2')
+        query_params = 'teste1'
+
+        get "/api/v1/books?author=#{query_params}", headers: get_headers(user)
+        
+        expect(json_body.count).to eq(1)
+      end
+    end
+  end
+  
   describe 'POST #create' do
     context 'when creating a book' do
       it 'return 201 status code and attributes' do
